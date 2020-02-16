da2b50d5d30d0557e1846d5db22bad7bba66c073
Lucas Cordeiro <lucascordeiroaraujo@gmail.com>
2020-02-16 17:34:27 -0300

    separate blogItem and use react memo

---
+++ components/index/blog/components/blogItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+import BlogItem from './style';
+
+const cpBlogItem: React.FC = () => (
+  <BlogItem>
+    <Link href="/sobre-nos">
+      <a href="sobre-nos" title="Confira">
+        <img src="https://picsum.photos/300/300?random=2" alt="" />
+      </a>
+    </Link>
+    <h2>
+      <Link href="/sobre-nos">
+        <a href="sobre-nos" title="Confira">
+          análise e recomendações
+        </a>
+      </Link>
+    </h2>
+    <p>
+      <Link href="/sobre-nos">
+        <a href="sobre-nos" title="Confira">
+          Fique por dentro dos melhores conteúdos de investimentos, seja você
+          iniciante ou especialista.
+        </a>
+      </Link>
+    </p>
+    <Link href="/sobre-nos">
+      <a href="/sobre-nos" title="Confira" className="read-more">
+        saiba mais..
+      </a>
+    </Link>
+  </BlogItem>
+);
+
+export default React.memo(cpBlogItem);
--- components/index/blog/index.tsx
+++ components/index/blog/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
 import Blog from './style';
+import BlogItem from './components/blogItem';
 
 const cpBlog: React.FC = () => (
   <Blog>
@@ -12,88 +12,9 @@ const cpBlog: React.FC = () => (
       </strong>
     </h1>
     <div>
-      <article>
-        <Link href="/sobre-nos">
-          <a href="sobre-nos" title="Confira">
-            <img src="https://picsum.photos/300/300" alt="" />
-          </a>
-        </Link>
-        <h2>
-          <Link href="/sobre-nos">
-            <a href="sobre-nos" title="Confira">
-              como começar a investir
-            </a>
-          </Link>
-        </h2>
-        <p>
-          <Link href="/sobre-nos">
-            <a href="sobre-nos" title="Confira">
-              Investir em uma das maiores instituições financeiras do país é
-              muito simples. Em poucos passos, você já estará pronto para
-              começar.
-            </a>
-          </Link>
-        </p>
-        <Link href="/sobre-nos">
-          <a href="/sobre-nos" title="Confira" className="read-more">
-            saiba mais..
-          </a>
-        </Link>
-      </article>
-      <article>
-        <Link href="/sobre-nos">
-          <a href="sobre-nos" title="Confira">
-            <img src="https://picsum.photos/300/300?random=1" alt="" />
-          </a>
-        </Link>
-        <h2>
-          <Link href="/sobre-nos">
-            <a href="sobre-nos" title="Confira">
-              educação
-            </a>
-          </Link>
-        </h2>
-        <p>
-          <Link href="/sobre-nos">
-            <a href="sobre-nos" title="Confira">
-              Aprenda mais sorbe os diversos tipos de investimentos e entenda
-              como seu dinheiro pode render de verdade.
-            </a>
-          </Link>
-        </p>
-        <Link href="/sobre-nos">
-          <a href="/sobre-nos" title="Confira" className="read-more">
-            saiba mais..
-          </a>
-        </Link>
-      </article>
-      <article>
-        <Link href="/sobre-nos">
-          <a href="sobre-nos" title="Confira">
-            <img src="https://picsum.photos/300/300?random=2" alt="" />
-          </a>
-        </Link>
-        <h2>
-          <Link href="/sobre-nos">
-            <a href="sobre-nos" title="Confira">
-              análise e recomendações
-            </a>
-          </Link>
-        </h2>
-        <p>
-          <Link href="/sobre-nos">
-            <a href="sobre-nos" title="Confira">
-              Fique por dentro dos melhores conteúdos de investimentos, seja
-              você iniciante ou especialista.
-            </a>
-          </Link>
-        </p>
-        <Link href="/sobre-nos">
-          <a href="/sobre-nos" title="Confira" className="read-more">
-            saiba mais..
-          </a>
-        </Link>
-      </article>
+      <BlogItem />
+      <BlogItem />
+      <BlogItem />
     </div>
   </Blog>
 );
