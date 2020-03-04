cc5d8f7b5198b339728277c4ae94671b28944793
Lucas Cordeiro <lucascordeiroaraujo@gmail.com>
2020-03-04 00:55:11 -0300

    remove next-routes

--- routes.js
+++
@@ -1,6 +0,0 @@
-const routes = require('next-routes');
-
-// Name   Page      Pattern
-module.exports = routes() // ----   ----      -----
-  .add('sobre-nos') // about  about     /about
-  .add('blog-post', '/blog/:slug');
--- server.js
+++ server.js
@@ -1,49 +1,35 @@
-// const express = require('express');
-
-// const next = require('next');
-
-// const dev = process.env.NODE_ENV !== 'production';
-
-// const app = next({ dev });
-
-// const handle = app.getRequestHandler();
-
-// app.prepare().then(() => {
-//   const server = express();
-
-//   server.get('/blog/:slug', (req, res) => {
-//     return app.render(req, res, '/blog-post', {
-//       slug: req.params.slug
-//     });
-//   });
-
-//   server.get('/blog/categoria/:slug', (req, res) => {
-//     return app.render(req, res, '/blog', {
-//       category: req.params.slug
-//     });
-//   });
-
-//   server.all('*', (req, res) => {
-//     return handle(req, res);
-//   });
-
-//   server.listen(3000, err => {
-//     if (err) throw err;
-
-//     console.log('🚀 Ready on http://localhost:3000');
-//   });
-// });
-
-// server.js
-const next = require('next');
-const routes = require('./routes');
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
-const handler = routes.getRequestHandler(app);
-
-// With express
 const express = require('express');
+
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+const app = next({ dev });
+
+const handle = app.getRequestHandler();
+
 app.prepare().then(() => {
-  express()
-    .use(handler)
-    .listen(3000);
+  const server = express();
+
+  server.get('/blog/:slug', (req, res) => {
+    return app.render(req, res, '/blog-post', {
+      slug: req.params.slug
+    });
+  });
+
+  server.get('/blog/categoria/:slug', (req, res) => {
+    return app.render(req, res, '/blog', {
+      category: req.params.slug
+    });
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, err => {
+    if (err) throw err;
+
+    console.log('🚀 Ready on http://localhost:3000');
+  });
 });
