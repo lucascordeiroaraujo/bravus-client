6a2ffcd20d97d534ec3e0e322a79385fd7a990be
Lucas Cordeiro <lucascordeiroaraujo@gmail.com>
2020-03-04 23:25:31 -0300

    POST create account form

--- components/index/callToAction/index.tsx
+++ components/index/callToAction/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import InputMask from 'react-input-mask';
 
@@ -9,6 +9,10 @@ import CallToAction from './style';
 import Fade from 'react-reveal/Fade';
 
 import { useRouter } from 'next/router';
+
+import { toggleFeedbackUser } from '~/store/global/actions';
+
+import { URL_API } from '~/utils/config';
 
 const cpCallToAction: React.FC = () => {
   const { error, data } = useSelector((state: any) => ({
@@ -22,19 +26,92 @@ const cpCallToAction: React.FC = () => {
 
   const router = useRouter();
 
+  const INITIAL_STATE = {
+    name: {
+      key: 'Nome',
+      value: ''
+    },
+    phone: {
+      key: 'Telefone',
+      value: ''
+    }
+  };
+
+  const [state, setState] = React.useState(INITIAL_STATE);
+
+  const dispatch = useDispatch();
+
+  const saveOpenAccount = async (event: any) => {
+    event.preventDefault();
+
+    dispatch(
+      toggleFeedbackUser('Aguarde, salvando informações', true, false, false)
+    );
+
+    const response = await fetch(`${URL_API}/bravus/saveOpenAccount`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      body: JSON.stringify({
+        body: Object.values(state)
+      })
+    });
+
+    if (response.status === 200) {
+      dispatch(
+        toggleFeedbackUser(
+          '<span class="emoji success">✔</span><br/> Informações salvas com sucesso.<br/> Em breve um de nossos assessores entratá em contato',
+          false,
+          false,
+          true
+        )
+      );
+
+      setState(INITIAL_STATE);
+    } else {
+      dispatch(
+        toggleFeedbackUser(
+          '<span class="emoji error">⚠</span><br/> Falha ao salvar suas informações.',
+          false,
+          true,
+          false
+        )
+      );
+    }
+  };
+
   return (
     <CallToAction id="abra-sua-conta">
       <Fade>
         <div dangerouslySetInnerHTML={{ __html: section_1_description }} />
       </Fade>
 
-      <form className={(router.asPath = '/#abra-sua-conta' ? 'active' : '')}>
+      <form
+        className={(router.asPath = '/#abra-sua-conta' ? 'active' : '')}
+        onSubmit={saveOpenAccount}
+      >
         <Fade bottom>
           <strong>Abra sua conta</strong>
         </Fade>
 
         <Fade bottom delay={200}>
-          <input type="text" placeholder="NOME" required />
+          <input
+            type="text"
+            placeholder="NOME"
+            required
+            value={state.name.value}
+            onChange={e =>
+              setState({
+                ...state,
+                name: {
+                  ...state.name,
+                  value: e.target.value
+                }
+              })
+            }
+          />
         </Fade>
 
         <Fade bottom delay={400}>
@@ -47,6 +124,16 @@ const cpCallToAction: React.FC = () => {
               9: '[0-9]',
               '?': '[0-9]'
             }}
+            value={state.phone.value}
+            onChange={e =>
+              setState({
+                ...state,
+                phone: {
+                  ...state.phone,
+                  value: e.target.value
+                }
+              })
+            }
           />
         </Fade>
 
