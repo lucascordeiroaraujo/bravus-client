f8c92dbb77cb4a00ef2a884c53ff3804ef127879
Lucas Cordeiro <lucascordeiroaraujo@gmail.com>
2020-04-02 00:44:09 -0300

    fix parallax

--- components/global/parallax/index.tsx
+++ components/global/parallax/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
-import { Parallax } from 'react-parallax';
-
 import Container from './style';
 
 import Fade from 'react-reveal/Fade';
@@ -22,23 +20,22 @@ const cpParallax: React.FC<Iprops> = ({ item, type }) => {
   if (error) return null;
 
   return (
-    <Container className={type}>
-      <Parallax
-        bgImage={data.acf[`${item}_image`].url}
-        bgImageAlt="Bravus"
-        strength={400}
-      >
-        <div>
-          <Fade bottom>
-            <p>{data.acf[`${item}_description`]}</p>
-          </Fade>
-          <Fade bottom delay={200}>
-            <span>{data.acf[`${item}_author`]}</span>
-          </Fade>
-        </div>
+    <Container
+      className={type}
+      style={{
+        backgroundImage: `url(${data.acf[`${item}_image`].url})`
+      }}
+    >
+      <img src={data.acf[`${item}_image`].url} alt="" />
 
-        <div style={{ height: '98vh' }} />
-      </Parallax>
+      <div>
+        <Fade bottom>
+          <p>{data.acf[`${item}_description`]}</p>
+        </Fade>
+        <Fade bottom delay={200}>
+          <span>{data.acf[`${item}_author`]}</span>
+        </Fade>
+      </div>
     </Container>
   );
 };
--- components/global/parallax/style.ts
+++ components/global/parallax/style.ts
@@ -3,44 +3,65 @@ import styled from 'styled-components';
 export default styled.div`
   width: calc(100% - 30px);
   margin: 60px 15px;
-  div.react-parallax-content {
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+  img {
+    width: 100%;
+    height: auto;
+    max-height: 90vh;
+    opacity: 0;
+  }
+  & > div {
+    position: absolute;
+    bottom: 9%;
+    left: 50%;
+    transform: translate(-50%, 0px);
     width: 100%;
     max-width: 1270px;
-    margin: 0px auto;
     display: flex;
-    justify-content: flex-end;
-    align-items: flex-end;
-    padding: 9% 0px;
-    & > div {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      p,
-      span {
-        width: 100%;
-        text-align: center;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    p,
+    span {
+      width: 100%;
+      text-align: center;
+    }
+    p {
+      font-family: 'DINProBold';
+      color: ${props => props.theme.colors.light};
+      margin: 0px 0px 50px 0px;
+      font-size: calc(1rem + ((1vw - 2.5px) * 2.994));
+      line-height: calc(1rem + ((1vw - 2.5px) * 2.994));
+      @media (min-width: 1920px) {
+        font-size: 66px;
+        line-height: 66px;
       }
-      p {
-        font-family: 'DINProBold';
-        color: ${props => props.theme.colors.light};
-        margin: 0px 0px 50px 0px;
-        font-size: calc(33px + (66 - 33) * ((100vw - 100px) / (1920 - 100)));
-        line-height: calc(33px + (66 - 33) * ((100vw - 100px) / (1920 - 100)));
-        @media (min-width: 1920px) {
-          font-size: 66px;
-          line-height: 66px;
-        }
-      }
-      span {
+    }
+    span {
+      font-size: calc(0.875rem + ((1vw - 2.5px) * 0.2395));
+      color: ${props => props.theme.colors.light};
+      text-transform: uppercase;
+      @media (min-width: 1920px) {
         font-size: 18px;
-        color: ${props => props.theme.colors.light};
-        text-transform: uppercase;
       }
     }
   }
-  &.second-index div.react-parallax-content {
-    align-items: center;
-    padding: 0px;
+  @media (max-width: 992px) {
+    background: none !important;
+    img {
+      opacity: 1;
+    }
+    & > div p {
+      margin: 0px 0px 20px 0px;
+    }
+  }
+  &.second-index > div {
+    top: 50%;
+    transform: translate(-50%, -50%);
+    bottom: auto;
   }
 `;
